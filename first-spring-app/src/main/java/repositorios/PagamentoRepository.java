2dcfbcabbb743a841f50c10d10700b1ca76f5d13
maria <dilmasoares649@gmail.com>
2025-01-28 20:29:37 -0300

    Merge remote-tracking branch 'origin/master'

--- first-spring-app/src/main/java/repositorios/PagamentoRepository.java
+++ first-spring-app/src/main/java/repositorios/PagamentoRepository.java
@@ -2,10 +2,14 @@ package repositorios;
 
 
 import model.Pagamento;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 
 public interface PagamentoRepository extends CrudRepository<Pagamento,Long> {
+
+
+
 }
--- first-spring-app/src/main/java/service/PagamentoService.java
+++ first-spring-app/src/main/java/service/PagamentoService.java
@@ -1,4 +1,22 @@
 package service;
 
+import model.Pagamento;
+import org.springframework.beans.factory.annotation.Autowired;
+import repositorios.PagamentoRepository;
+
 public class PagamentoService {
+
+
+  @Autowired
+  private PagamentoRepository pagamentoRepository;
+
+  public Pagamento novoPagamento (Pagamento pagamento ){
+
+     return  pagamentoRepository.save(pagamento);
+  }
+
+
+
+
+
 }
