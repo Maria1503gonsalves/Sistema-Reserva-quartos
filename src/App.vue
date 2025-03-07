f57963f56fd9c651605f945bc59b529509733c6e
maria <dilmasoares649@gmail.com>
2025-03-07 20:36:06 -0300

    criei a pagina de escolher datas de entrada e saida

--- src/App.vue
+++ src/App.vue
@@ -7,47 +7,61 @@
       País: {{ country }}
     </p>
     <button @click="getLocation">Obter Localização</button>
+
+    <h2>encontre seu melhor quarto</h2>
+    <flat-pickr 
+      v-model="dateRange" 
+      :config="config" 
+      placeholder="Selecione as datas" 
+    />
+    <p>Data de entrada: {{ dateRange[0] }}</p>
+    <p>Data de saída: {{ dateRange[1] }}</p>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import FlatPickr from 'vue-flatpickr-component';
+import 'flatpickr/dist/flatpickr.css';
 
 export default {
+  components: {
+    FlatPickr,
+  },
   data() {
     return {
-      city: null, // Nome da cidade
-      country: null, // Nome do país
-      errorMessage: "", // Para mensagens de erro
+      city: null,
+      country: null,
+      errorMessage: "",
+      dateRange: [],
+      config: {
+        mode: "range",
+        dateFormat: "Y-m-d",
+      },
     };
   },
   methods: {
     async getLocation() {
-      // Verifica se o navegador suporta a API de Geolocalização
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
           async (position) => {
             const { latitude, longitude } = position.coords;
 
             try {
-              // Faz uma requisição à API do OpenCage
               const response = await axios.get(
-                
-  `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=20fb2055e4e643efa8908a10e26338c3`
+                `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=20fb2055e4e643efa8908a10e26338c3`
               );
 
-              // Extrai a cidade e o país da resposta
               const components = response.data.results[0].components;
-              this.city = components.city || components.town || components.village;
+              this.city =
+                components.city || components.town || components.village;
               this.country = components.country;
               this.errorMessage = "";
-
             } catch (error) {
               this.errorMessage = "Erro ao obter a localização: " + error.message;
             }
           },
           (error) => {
-            // Tratamento de erros de geolocalização
             switch (error.code) {
               case error.PERMISSION_DENIED:
                 this.errorMessage = "Permissão negada pelo usuário.";
@@ -70,18 +84,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-button {
-  margin-top: 10px;
-  padding: 10px 20px;
-  background-color: #42b883;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-}
-button:hover {
-  background-color: #339c70;
-}
-</style>
